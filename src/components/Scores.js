5ec177026f0a6e3acc834038705877218870082a
Helen Liutongco <liutongcoh@gmail.com>
2019-03-18 01:21:40 -0400

    edit readme

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-class Scores extends React.Component {
-
-  render() {
-    return (
-      <h2 id="score-text">Score: {this.props.score}</h2>
-    )
-  }
+const Scores = (props) => {
+  return (
+    <h2 id="score-text">Score: {props.score}</h2>
+  )
 }
 
 const mapStateToProps = (state) => {
