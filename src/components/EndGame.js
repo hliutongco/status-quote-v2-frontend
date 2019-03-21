dc6c8e937a4b937c7f866dc0ce439914d86d36eb
Helen <liutongcoh@gmail.com>
2019-03-21 12:16:56 -0400

    Refactor EndGame

--- src/components/EndGame.js
+++ src/components/EndGame.js
@@ -1,24 +1,26 @@
-import React, {Component} from 'react'
+import React, {Fragment, useState, useEffect} from 'react'
 import {changeDisplay, changeGameStatus, updateScore} from '../actions'
 import {connect} from 'react-redux'
 
-class EndGame extends Component {
-  componentWillUnmount(){
-      this.props.handleRestart(null)
-      this.props.updateScore(0)
-  }
 
-  render(){
-    return (
-      <div id="end-game">
-        <h1>Game Over!</h1>
-        <h2>Final Score:</h2>
-        <h2>{this.props.score}</h2>
-        <button onClick={() => this.props.handleChange(null)} className='start-btn'>Main Menu</button>
-      </div>
-    )
-  }
+const EndGame = (props) => {
+  useEffect(() => {
+    return () => {
+       props.handleRestart(null)
+       props.updateScore(0)
+   }
+  })
+
+  return (
+    <div id="end-game">
+      <h1>Game Over!</h1>
+      <h2>Final Score:</h2>
+      <h2>{props.score}</h2>
+      <button onClick={() => props.handleChange(null)} className='start-btn'>Main Menu</button>
+    </div>
+  )
 }
+
 
 const mapStateToProps = (state) => {
   return {
