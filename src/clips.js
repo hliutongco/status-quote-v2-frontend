f267c828ae2f0957323e21e66161a4cc589ec0cc
Helen Liutongco <liutongcoh@gmail.com>
2019-03-21 22:10:49 -0400

    Add new urls

--- src/clips.js
+++ src/clips.js
@@ -2,7 +2,7 @@ export const clips = [
   {
     id: 1,
     title: 'A League of Their Own',
-    link: 'https://drive.google.com/uc?id=1PpV-1H3CSrs3ewHuOkEvIQLIWVHctWhH',
+    link: 'http://localhost:3000/clip1',
     quote: 'there\'s no crying in baseball',
     hint: 'no crying',
     time: 10000
@@ -10,7 +10,7 @@ export const clips = [
   {
     id: 2,
     title: 'Back to the Future 2',
-    link: 'https://drive.google.com/uc?id=1cE5TpS70IzZAbaM4T6mD7nUVrSW5pkOM',
+    link: 'http://localhost:3000/clip2',
     quote: 'where we\'re going we don\'t need roads',
     hint: 'don\'t need roads',
     time: 9000
@@ -18,7 +18,7 @@ export const clips = [
   {
     id: 3,
     title: 'A Few Good Men',
-    link: 'https://drive.google.com/uc?id=11_00hlcxRkB_wcgfvJ1rN50noaHN7YIN',
+    link: 'http://localhost:3000/clip3',
     quote: 'you can\'t handle the truth',
     hint: 'can\'t handle',
     time: 5000
@@ -26,7 +26,7 @@ export const clips = [
   {
     id: 4,
     title: 'Star Wars: The Empire Strikes Back',
-    link: 'https://drive.google.com/uc?id=1QEBSxyChMLigatKLYerrfj4b-6jFxCoI',
+    link: 'http://localhost:3000/clip4',
     quote: 'try not do or do not there is no try',
     hint: 'try not',
     time: 8000
